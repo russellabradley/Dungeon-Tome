b0e2a696fecf6fca35e9f03d5411318cdde61183
Russell Bradley <russellabradley@gmail.com>
2017-08-02 19:06:31 -0400

    got api and fetch working for campaigns index page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
 
   root 'static_pages#index'
-
   get '/campaign', to: 'static_pages#index'
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :campaigns
+    end
+  end
+
 
 end
