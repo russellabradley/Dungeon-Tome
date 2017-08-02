337a89e4a6ac26cb36cf4db6d2e55e9c0d4215ce
Russell Bradley <russellabradley@gmail.com>
2017-08-02 11:13:54 -0400

    added user migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731185126) do
+ActiveRecord::Schema.define(version: 20170802145416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,11 @@ ActiveRecord::Schema.define(version: 20170731185126) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["campaign_id"], name: "index_sessions_on_campaign_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
