b0abfefd5cd045c066acb0aff33b644d0913fda2
Russell Bradley <russellabradley@gmail.com>
2017-08-04 17:00:16 -0400

    got user creation and auth working finally

--- app/javascript/react/src/components/Login.js
+++ app/javascript/react/src/components/Login.js
@@ -50,40 +50,11 @@ export default class Login extends React.Component {
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }
 
-  // handleSignup (event) {
-  //   event.preventDefault()
-  //   fetch('/api/v1/users_controller/', {
-  //     headers: {'Content-Type': 'application/json'},
-  //     method: 'POST',
-  //     body: JSON.stringify({"auth": {"email": this.state.signupEmail, "password": this.state.signupPassword}})
-  //   })
-  //   .then(response => {
-  //     if (response.ok) {
-  //       return response;
-  //     } else {
-  //       let errorMessage = `${response.status} (${response.statusText})`,
-  //         error = new Error(errorMessage);
-  //       throw(error);
-  //     }
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     if (data.jwt) {
-  //       window.localStorage.token = data.jwt
-  //       this.props.history.push('/campaigns')
-  //     } else {
-  //       // set state error true and show a div that says bad login
-  //     }
-  //   })
-  //   .catch(error => console.error(`Error in fetch: ${error.message}`))
-  // }
-
-
   handleSignup(event) {
     event.preventDefault();
     fetch('/api/v1/users', {
       method: 'POST',
-      body: JSON.stringify({email: this.state.signupEmail, password_digest: this.state.signupPassword})
+      body: JSON.stringify({email: this.state.signupEmail, password: this.state.signupPassword})
     }).then(response => {
     	if (response.ok) {
     	  return response;
